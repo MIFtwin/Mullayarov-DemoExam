59d82d46dbaf46d48c986f3a6a2b69f2c9e7802d
Ильгиз <the.mullayarov.twins@gmail.com>
2024-11-14 11:14:08 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import datetime
+from pydantic import BaseModel
+
+
+class Order(BaseModel):
+    number : int
+    starDate : datetime.date
+    device : str
+    problemType : str
+    description : str
+    client : str
+    status : str
+
+
+
+from fastapi import FastAPI
+
+ 
+app = FastAPI()
+ 
+@app.get("/")
+def read_root():
+    
+    return "Hello"
